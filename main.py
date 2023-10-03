dbd254c6ed6be08378458784118f6393f5c8d3d3
Mati <mailrandom@gmail.com>
2023-10-03 18:17:28 -0300

    a

---
+++ main.py
@@ -0,0 +1,13 @@
+# Este script sera para lo principal del programa, por no decir todo jajaja, es muy basico.
+
+import urllib.request
+
+def main():
+    target = input("Ingrese la URL: ");
+    try:
+        response = urllib.request.urlopen(target);
+        print(response.read());
+    except: 
+        print("Error al abrir la URL");
+
+
